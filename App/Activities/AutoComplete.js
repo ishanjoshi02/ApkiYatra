a0193dbab10f904331806c1944f0f245667ae369
Chinmay Kulkarni <chinmay@sellezely.com>
2018-08-26 16:22:49 +0530

    Implemented: Location Select

--- App/Activities/AutoComplete.js
+++ App/Activities/AutoComplete.js
@@ -1,120 +1,19 @@
 import Autocomplete from "react-native-autocomplete-input";
 import React, { Component } from "react";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
-
-const API = "https://swapi.co/api";
-const ROMAN = ["", "I", "II", "III", "IV", "V", "VI", "VII"];
-
-class AutocompleteExample extends Component {
-  static renderFilm(film) {
-    const { title, director, opening_crawl, episode_id } = film;
-    const roman = episode_id < ROMAN.length ? ROMAN[episode_id] : episode_id;
-
-    return (
-      <View>
-        <Text style={styles.titleText}>
-          {roman}. {title}
-        </Text>
-        <Text style={styles.directorText}>({director})</Text>
-        <Text style={styles.openingText}>{opening_crawl}</Text>
-      </View>
-    );
-  }
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      films: [],
-      query: "",
-      data: []
-    };
-  }
-
-  componentDidMount() {
-    fetch(`${API}/films/`)
-      .then(res => res.json())
-      .then(json => {
-        const { results: films } = json;
-        this.setState({ films });
-      });
-  }
-
-  findFilm(query) {
-    if (query === "") {
-      return [];
-    }
-
-    const { films } = this.state;
-    const regex = new RegExp(`${query.trim()}`, "i");
-    return films.filter(film => film.title.search(regex) >= 0);
-  }
-
-  onChangeText = async text => {
-    const locationsRequest = new Promise((resolve, reject) => {
-      fetch(
-        `https://maps.googleapis.com/maps/api/place/autocomplete/xml?input=${text}&types=establishment&key=AIzaSyCfLbyBRSOQX6RQrwyYc0KX9bRCfVtbgXw`
-      )
-        .then(request => request.json())
-        .catch(e => reject(e))
-        .then(data => {
-          resolve(data);
-        })
-        .catch(error => {
-          reject(error);
-        });
-    });
-
-    try {
-      const location = await locationsRequest;
-      console.log(location);
-    } catch (e) {
-      console.log(e);
-    }
-
-    this.setState({
-      query: text,
-      data: [
-        {
-          title: text
-        }
-      ]
-    });
-  };
-
-  render() {
-    const { query } = this.state;
-    const films = this.findFilm(query);
-    const comp = (a, b) => a.toLowerCase().trim() === b.toLowerCase().trim();
-
-    return (
-      <Autocomplete
-        autoCapitalize="none"
-        autoCorrect={false}
-        containerStyle={styles.autocompleteContainer}
-        data={this.state.data}
-        defaultValue={query}
-        onChangeText={this.onChangeText}
-        placeholder="Enter Your Destination"
-        renderItem={({ title, release_date }) => (
-          <TouchableOpacity onPress={() => this.setState({ query: title })}>
-            <Text style={styles.itemText}>{title}</Text>
-          </TouchableOpacity>
-        )}
-      />
-    );
-  }
-}
-
 const styles = StyleSheet.create({
   container: {
     backgroundColor: "#F5FCFF",
     flex: 1,
-    paddingTop: 0,
-    height: 50,
+    paddingTop: 25
   },
   autocompleteContainer: {
-    marginLeft: 0,
-    marginRight: 0
+    flex: 1,
+    left: 0,
+    position: "absolute",
+    right: 0,
+    top: 0,
+    zIndex: 1
   },
   itemText: {
     fontSize: 15,
@@ -124,7 +23,7 @@ const styles = StyleSheet.create({
     // `backgroundColor` needs to be set otherwise the
     // autocomplete input will disappear on text input.
     backgroundColor: "#F5FCFF",
-    marginTop: 8
+    marginTop: 25
   },
   infoText: {
     textAlign: "center"
@@ -146,5 +45,126 @@ const styles = StyleSheet.create({
     textAlign: "center"
   }
 });
+const API = "https://swapi.co/api";
+const ROMAN = ["", "I", "II", "III", "IV", "V", "VI", "VII"];
+const locationsRequest = text =>
+  new Promise((resolve, reject) => {
+    fetch(
+      `https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${text}&types=geocode&language=fr&key=AIzaSyAKUhmKuJ6pVg7FrdnAcmubDdfE6k-D4uw`
+    )
+      .then(request => request.json())
+      .catch(e => reject(e))
+      .then(data => {
+        resolve(
+          data.predictions.map(r => ({
+            title: r.structured_formatting.main_text,
+            place_id: r.place_id
+          }))
+        );
+      })
+      .catch(error => {
+        reject(error);
+      });
+  });
+class AutocompleteExample extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      films: [],
+      query: "",
+      data: [],
+      hideResults: false
+    };
+  }
+
+  onChangeText = async text => {
+    try {
+      const locations = await locationsRequest(text);
+      this.setState({
+        hideResults: false,
+        query: text,
+        data: locations
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  onSelect = selectedLocation => {
+    this.setState({
+      query: selectedLocation.title,
+      hideResults: true
+    });
+    if (this.props.handleLocationSelect) {
+      this.props.handleLocationSelect(selectedLocation);
+    }
+  };
+
+  render() {
+    const { query } = this.state;
+    const comp = (a, b) => a.toLowerCase().trim() === b.toLowerCase().trim();
+
+    return (
+      <View style={styles.autocompleteContainer}>
+        <Autocomplete
+          autoCapitalize="none"
+          autoCorrect={false}
+          data={this.state.data}
+          defaultValue={query}
+          hideResults={this.state.hideResults}
+          onChangeText={this.onChangeText}
+          placeholder="Enter Your Destination"
+          renderItem={locationItem => (
+            <TouchableOpacity onPress={() => this.onSelect(locationItem)}>
+              <Text style={styles.itemText}>{locationItem.title}</Text>
+            </TouchableOpacity>
+          )}
+        />
+      </View>
+    );
+  }
+}
+
+// const styles = StyleSheet.create({
+//   container: {
+//     backgroundColor: "#F5FCFF",
+//     flex: 1,
+//     paddingTop: 0,
+//     height: 50
+//   },
+//   autocompleteContainer: {
+//     marginLeft: 0,
+//     marginRight: 0
+//   },
+//   itemText: {
+//     fontSize: 15,
+//     margin: 2
+//   },
+//   descriptionContainer: {
+//     // `backgroundColor` needs to be set otherwise the
+//     // autocomplete input will disappear on text input.
+//     backgroundColor: "#F5FCFF",
+//     marginTop: 8
+//   },
+//   infoText: {
+//     textAlign: "center"
+//   },
+//   titleText: {
+//     fontSize: 18,
+//     fontWeight: "500",
+//     marginBottom: 10,
+//     marginTop: 10,
+//     textAlign: "center"
+//   },
+//   directorText: {
+//     color: "grey",
+//     fontSize: 12,
+//     marginBottom: 10,
+//     textAlign: "center"
+//   },
+//   openingText: {
+//     textAlign: "center"
+//   }
+// });
 
 export default AutocompleteExample;
